e712ad046586198a382d2d5732d6555fc4ef14ee
kennycro <kennyadelowo@gmail.com>
2023-04-13 10:02:09 -0700

    task 0-3

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -5,7 +5,6 @@ void _print(char *str, int l);
 char *mul(char n, char *num, int num_index, char *dest, int dest_index);
 int check_for_digits(char **av);
 void init(char *str, int l);
-#include <limit.h>
 #include <stdio.h>
 #include <stdlib.h>
 int _putchar(char c);
